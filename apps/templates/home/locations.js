a95870ccf6c0814bd8e3c0706e9a653b3c5a7b8c
Matthew Jung <90943304+MatthewJung14@users.noreply.github.com>
2024-10-26 23:46:48 -0400

    Updated Map

--- apps/templates/home/locations.js
+++ apps/templates/home/locations.js
@@ -1,5 +1,9 @@
 class Locations {
-    constructor(donationLocations, neededLocations) {
+
+    donationLocations = [];
+    neededLocations = [];
+
+    constructor(donationLocations = [], neededLocations = []) {
         this.donationLocations = donationLocations;
         this.neededLocations = neededLocations;
     }
@@ -44,19 +48,6 @@ class Locations {
         return degrees * (Math.PI / 180);
     }
 }
-
-// Define your locations here
-const donationLocations = [
-    { name: "Food Bank Miami", coords: [25.7617, -80.1918] },
-    { name: "Habitat for Humanity Tampa", coords: [27.9506, -82.4572] },
-    { name: "Community Foundation Jacksonville", coords: [30.3322, -81.6557] }
-];
-
-const neededLocations = [
-    { name: "Shelter Orlando", coords: [28.5383, -81.3792] },
-    { name: "Assistance League Sarasota", coords: [27.3369, -82.5333] },
-    { name: "Red Cross Gainesville", coords: [29.6516, -82.3248] }
-];
 
 // Export an instance of the Locations class
 export default new Locations(donationLocations, neededLocations);
